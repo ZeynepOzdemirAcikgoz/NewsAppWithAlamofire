4fba295ab863f800624ff41293d7ffc9783680eb
Zeynep Özdemir Açıkgöz <zeynmirgmail.com>
2022-09-29 11:50:54 +0300

    Initial Commit

---
+++ NewsAppSwiftUI/NewsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppSwiftUIApp.swift
+//  NewsAppSwiftUI
+//
+//  Created by Zeynep Özdemir Açıkgöz on 29.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
